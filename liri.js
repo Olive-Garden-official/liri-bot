29c3102b4c15f7ffd47ac4e5825985338c1e7dd3
Addison Nance <madi.nance911@gmail.com>
2020-05-16 13:02:18 -0700

    pushing so I can pull

--- liri.js
+++ liri.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 
 var keys= require("./keys.js");
 
-// var spotify = new spotify(keys.spotify);
+// var spotify = new Spotify(keys.spotify);
 
 
 var command = process.argv[2];
